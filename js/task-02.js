0a23e1e7653f2a4c0cbc1c4a458bd21773da0e79
Sergey Gulaga <ry6ens@gmail.com>
2022-07-04 22:13:01 +0300

    done task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,20 +7,10 @@ const ingredients = [
   "Condiments",
 ];
 
-const listIngridients = document.querySelector("#ingredients");
-const itemIngridient = document.createElement("li");
-itemIngridient.classList.add("item");
-itemIngridient.textContent = "Potatoes";
-listIngridients.append(itemIngridient);
-
-// for (let i = 0; i < ingredients.length; i++) {
-//   console.log(i);
-//   document.createElement("li").textContent = ingredients[i];
-// }
-
-for (const key of ingredients) {
-  console.log(key);
-  itemIngridient.classList.add("item");
-  itemIngridient.textContent = key;
+for (let i = 0; i < ingredients.length; i++) {
+  const listIngridients = document.querySelector("#ingredients");
+  const itemIngridient = document.createElement("li");
   listIngridients.append(itemIngridient);
+  itemIngridient.classList.add("item");
+  itemIngridient.textContent = ingredients[i];
 }
